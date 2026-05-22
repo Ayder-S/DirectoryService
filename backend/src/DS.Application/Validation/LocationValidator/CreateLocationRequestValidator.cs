8dfb18e858ed7c15cca3b387fd22031896177c11
AyderSettarov <asettarov545@gmail.com>
2026-05-22 23:26:33 +0500

    Использовал fluentvalidation и добавил валидацию для создания Локации в хендлере

---
+++ backend/src/DS.Application/Validation/LocationValidator/CreateLocationRequestValidator.cs
@@ -0,0 +1,19 @@
+﻿using DS.Application.Validation;
+using DS.Contracts.Locations.Create;
+using DS.Domain.ValueObjects;
+using FluentValidation;
+using Shared.Constants;
+
+namespace DS.Application.Validators.LocationValidator;
+
+public class CreateLocationRequestValidator : AbstractValidator<CreateLocationRequest>
+{
+    public CreateLocationRequestValidator()
+    {
+        RuleFor(l => l.Name)
+            .MustBeValueObject(Name.Create);
+
+        // RuleFor(l => l.Address)
+        //     .MustBeValueObject(Address.Create());
+    }
+}
---
+++ backend/src/DS.Application/Validation/ValidationExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json;
+using FluentValidation.Results;
+using Microsoft.Extensions.Validation;
+using Shared.AppFails;
+
+namespace DS.Application.Validation;
+
+public static class ValidationExtensions
+{
+    public static ErrorsList ToErrors(this ValidationResult validationResult)
+    {
+        var errors = validationResult.Errors
+            .Select(e => JsonSerializer.Deserialize<Error>(e.ErrorMessage)!)
+            .ToList();
+        
+        return new ErrorsList(errors);
+    }
+}
